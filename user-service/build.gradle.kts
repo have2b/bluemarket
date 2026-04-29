46638a5b311224f16fcaca74725803a5995f6893
have2b <thanhlongvu156@gmail.com>
2026-04-29 14:27:30 +0700

    feat: add base entity

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -13,6 +13,7 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+    implementation("io.quarkus:quarkus-hibernate-reactive-panache")
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-arc")
     testImplementation("io.quarkus:quarkus-junit")
---
+++ user-service/src/main/java/domain/entities/BaseEntity.java
@@ -0,0 +1,36 @@
+package domain.entities;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+
+@MappedSuperclass
+public abstract class BaseEntity extends PanacheEntityBase {
+    @Id
+    private UUID id;
+    @Column(updatable = false)
+    private Timestamp createdAt;
+    @Column(updatable = false)
+    private Timestamp updatedAt;
+
+    @PrePersist
+    protected void prePersist() {
+        if (id == null) {
+            id = UUID.randomUUID();
+        }
+        long now = System.currentTimeMillis();
+        createdAt = new Timestamp(now);
+        updatedAt = new Timestamp(now);
+    }
+
+    @PreUpdate
+    protected void preUpdate() {
+        updatedAt = new Timestamp(System.currentTimeMillis());
+    }
+}
