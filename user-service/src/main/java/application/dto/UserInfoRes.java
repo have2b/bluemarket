7f80dbd5802f186470989ca38363b5e25974d801
have2b <thanhlongvu156@gmail.com>
2026-04-29 20:15:04 +0700

    feat: add base interface for usecase

---
+++ user-service/src/main/java/application/dto/UserInfoRes.java
@@ -0,0 +1,5 @@
+package application.dto;
+
+public record UserInfoRes() {
+
+}
---
+++ user-service/src/main/java/application/usecases/IUserUseCase.java
@@ -0,0 +1,9 @@
+package application.usecases;
+
+import java.util.UUID;
+
+import application.dto.UserInfoRes;
+
+public interface IUserUseCase extends IBaseUseCase<UserInfoRes, UUID> {
+
+}
